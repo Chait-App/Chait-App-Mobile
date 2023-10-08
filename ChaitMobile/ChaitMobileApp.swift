ef614f352e18ce8352b3e6628f48518520e2216c
oguz56785 <oguzyildirim@posta.mu.edu.tr>
2023-10-08 22:51:12 +0300

    configure VIPER architecture

---
+++ ChaitMobile/ChaitMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaitMobileApp.swift
+//  ChaitMobile
+//
+//  Created by Oğuz Yıldırım on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChaitMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainPageView()
+        }
+    }
+}
---
+++ ChaitMobile/Chat/View/MainPageView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChaitMobile
+//
+//  Created by Oğuz Yıldırım on 8.10.2023.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainPageView()
+}
