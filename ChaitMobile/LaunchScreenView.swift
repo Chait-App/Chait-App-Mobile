603294d3637d465ad1dec211fb2d5828d80cd81c
Oğuz Yıldırım <oguzyildirim@posta.mu.edu.tr>
2023-10-08 23:01:51 +0300

    update VIPER architecture (#2)
    * configure VIPER architecture
    
    * update VIPER architecture

---
+++ ChaitMobile/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  ChaitMobile
+//
+//  Created by Oğuz Yıldırım on 8.10.2023.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text("Launch screen view!")
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
---
+++ ChaitMobile/MainPageView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChaitMobile
+//
+//  Created by Oğuz Yıldırım on 8.10.2023.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        VStack {
+            Text("Main page view!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainPageView()
+}
