07e08f3c484d07f714b978261b298d501944c00b
oguz56785 <oguzyildirim@posta.mu.edu.tr>
2023-10-12 19:58:07 +0300

    add helpers folder

---
+++ ChaitMobile/Helpers/ScreenSizeHelper.swift
@@ -0,0 +1,15 @@
+//
+//  ScreenSizeHelper.swift
+//  ChaitMobile
+//
+//  Created by Oğuz Yıldırım on 12.10.2023.
+//
+
+import SwiftUI
+
+struct ScreenSizeHelper {
+  static let screenWidth: CGFloat = UIScreen.main.bounds.width
+  static let screenHeight: CGFloat = UIScreen.main.bounds.height
+}
+
+
